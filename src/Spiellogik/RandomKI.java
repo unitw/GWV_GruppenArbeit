4c6bfd517e77ab2976629fe904ad3b3c8ee21478
TheChrisED <Chris@dhcp-172-21-0-167.wlan.uni-hamburg.de.local>
2015-01-21 13:23:10 +0100

    RandomKI eingefügt
    Zum Testen eine KI eingefügt, die zufällig einen Zug zieht.

---
+++ src/Spiellogik/RandomKI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Spiellogik;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author Chris
+ */
+public class RandomKI extends KI {
+
+    @Override
+    public Zug entscheide(List<Zug> zuege) {
+        Random random = new Random();
+        int zugIndex = random.nextInt(zuege.size());
+        return zuege.get(zugIndex);
+    }
+    
+}
